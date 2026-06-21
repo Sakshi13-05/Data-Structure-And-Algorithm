818a9385f75f9d3facfba02b106b46a1e601ca60
Sakshi13-05 <sakshichavan61116@gmail.com>
2026-06-21 21:34:26 +0530

    corrected the logic of valid anagram

--- LeetCode/ValidAnagram.java
+++ LeetCode/ValidAnagram.java
@@ -10,11 +10,15 @@ public class ValidAnagram {
         for (int i = 0; i < 26; i++) {
             freq[i] = 0;
         }
-        for (int i = 0; i < s.length(); i++) {
-            freq[i]++;
+
+        for (int i = 0; i < t.length(); i++) {
+            int index = s.charAt(i) - 97;
+            freq[index]++;
         }
-        for (int i = 0; i < s.length(); i++) {
-            freq[i]--;
+
+        for (int i = 0; i < t.length(); i++) {
+            int index = t.charAt(i) - 97;
+            freq[index]--;
 
         }
         for (int i = 0; i < 26; i++) {
@@ -26,7 +30,7 @@ public class ValidAnagram {
     }
 
     public static void main(String[] args) {
-        boolean ans = isAnagram("anagram", "car");
+        boolean ans = isAnagram("rat", "car");
         System.out.println(ans);
     }
 }
