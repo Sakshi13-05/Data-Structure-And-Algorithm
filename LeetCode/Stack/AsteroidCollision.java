ceb8f373ec3d3f22b2eda23f40cb1906c71361dd
Sakshi13-05 <sakshichavan61116@gmail.com>
2026-08-12 10:02:48 +0530

    Implemented survive variable method as continue failed now attempting to make code structure clean --Attempt2

--- LeetCode/Stack/AsteroidCollision.java
+++ LeetCode/Stack/AsteroidCollision.java
@@ -5,36 +5,42 @@ import java.util.Deque;
 
 public class AsteroidCollision {
     public static void main(String[] args) {
-        int[] asteroids = { 10, 2, -5 };
+        int[] asteroids = { 3, 5, -6, 2, -1, 4 };
 
         Deque<Integer> stack = new ArrayDeque<>();
         for (int i = 0; i < asteroids.length; i++) {
 
             int current = asteroids[i];
+            int current_abs = Math.abs(current);
+            boolean survived = true;
 
-            if (!stack.isEmpty() && (stack.peek() > 0 && current < 0)) {
-                int current_abs = Math.abs(current);
+            while (!stack.isEmpty() && (stack.peek() > 0 && current < 0)) {
 
                 if (current_abs == stack.peek()) {
                     stack.pop();
-                    continue;
+                    survived = false;
+                    break;
 
                 } else if (current_abs < stack.peek()) {
-                    continue;
+                    survived = false;
+                    break;
                 }
 
                 while (!stack.isEmpty() && current_abs > stack.peek()) {
                     stack.pop();
+
                 }
 
             }
-            stack.push(current);
+
+            if (survived) {
+                stack.push(current);
+            }
 
         }
 
         while (!stack.isEmpty()) {
             System.out.println(stack.pop());
-
         }
     }
 }
